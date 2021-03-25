2bb6b26827dc75acf1db77ba293e31c81809dde4
kalyssao <kalyssaowusu@gmail.com>
2021-03-25 17:55:49 +0000

    added order service provider

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { OrderService } from './services/order.service';
 
 const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [OrderService]
 })
 export class AppRoutingModule { }
