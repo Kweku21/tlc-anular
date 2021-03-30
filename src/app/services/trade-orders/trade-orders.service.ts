fe5fd5999147a53841de67ccb1843a5ba346c995
kalyssao <kalyssaowusu@gmail.com>
2021-03-30 12:00:47 +0000

    removed imports

--- src/app/services/trade-orders/trade-orders.service.ts
+++ src/app/services/trade-orders/trade-orders.service.ts
@@ -1,9 +1,7 @@
 import { Injectable } from '@angular/core';
-import { ClientOrder } from '../../domain/client-order/client-order'
-import { Trade } from '../../domain/trade/trade'
+import { ClientOrder } from '../../domain/client-order'
 import {HttpClient} from "@angular/common/http"
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
