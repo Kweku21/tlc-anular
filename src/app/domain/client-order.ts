515640efa077572ff6e14572b58ff60c05d305d9
kalyssao <kalyssaowusu@gmail.com>
2021-03-30 11:02:18 +0000

    added to domain

---
+++ src/app/domain/client-order.ts
@@ -0,0 +1,11 @@
+export class ClientOrder {
+    clientorderid: number;
+    product: string;
+    price: number;
+    quantity: number;
+    side: string;
+    validationstatus: string;
+    status: string;
+    clientid: number;
+    portfolioid: number;
+}
