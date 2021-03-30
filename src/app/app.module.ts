36cf7c10716044d5298c0cddeefe3ced25b4bd91
ValentineEssuman <jusvaless@gmail.com>
2021-03-30 12:16:01 +0100

    added balance to portfolio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-<<<<<<< HEAD
-import { FormsModule } from "@angular/forms";
-import { HttpClientModule } from '@angular/common/http';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { LoginFormComponent } from './components/login-form/login-form.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { BalanceComponent } from './components/balance/balance.component';
-
-=======
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
@@ -20,21 +9,20 @@ import { RegisterFormComponent } from './components/register-form/register-form.
 import { BodyComponent } from './components/body/body.component';
 import { PortfolioComponent } from './components/portfolio/portfolio.component';
 import { ProductComponent } from './components/product/product.component';
->>>>>>> debrah
+
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { BalanceComponent } from './components/balance/balance.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginFormComponent,
-<<<<<<< HEAD
-    NavbarComponent,
-    BalanceComponent,
-=======
     RegisterFormComponent,
     BodyComponent,
     PortfolioComponent,
-    ProductComponent
->>>>>>> debrah
+    ProductComponent,
+    NavbarComponent,
+    BalanceComponent,
   ],
   imports: [
     BrowserModule,
