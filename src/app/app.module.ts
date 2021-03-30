9f65d0c7205f42ef0f30c585956388189abf0eeb
kalyssao <kalyssaowusu@gmail.com>
2021-03-30 12:00:12 +0000

    new installs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule  } from "@angular/forms";
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,16 +9,19 @@ import { LoginFormComponent } from './components/login-form/login-form.component
 import { PlaceOrderComponent } from './components/place-order/place-order.component';
 import { HttpClientModule } from '@angular/common/http';
 import { MarketDataComponent } from './components/market-data/market-data.component';
+import { TradeHistoryComponent } from './components/trade-history/trade-history.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginFormComponent,
     PlaceOrderComponent,
-    MarketDataComponent
+    MarketDataComponent,
+    TradeHistoryComponent
   ],
   imports: [
     BrowserModule,
+    NgbModule,
     AppRoutingModule,
     FormsModule,
     HttpClientModule,
